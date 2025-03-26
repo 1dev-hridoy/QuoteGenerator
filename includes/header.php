99f2b2412ae4effe77f5db1e7a07fbae3d7b0d73
Hridoy <154232641+1dev-hridoy@users.noreply.github.com>
2025-03-26 23:41:08 +0600

    Remove unnecessary body classes from header.php

--- includes/header.php
+++ includes/header.php
@@ -8,5 +8,3 @@
     <script src="assets/js/style.js"></script>
     <link rel="stylesheet" href="assets/css/style.css">
     </head>
-    <body class="bg-gray-100 min-h-screen flex items-center justify-center p-4 font-sans">
-    <div class="max-w-2xl w-full">
